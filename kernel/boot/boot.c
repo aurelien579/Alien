b992139738ab44d697074df1a7be64188ec56f49
aurelien <aurlienmartin@gmail.com>
2019-01-30 20:54:54 +0100

    ata_read_sector works

--- kernel/boot/boot.c
+++ kernel/boot/boot.c
@@ -31,11 +31,19 @@ void kernel_main(struct mb_info *boot_info)
     
     ata_install();
     
-    struct device *dev = device_find("ATA-0");
-    uint32_t size = 1;
-    uint8_t out[4096];
-    
+    struct device *dev = device_find("ATA-2");
+    uint32_t size = 1024;
+    uint8_t out[4096];    
+
     device_random_read(dev, 0, &size, out);
+
+    printf("DATA: %s\n", out);
+    if (strcmp(out, "BONJOUR", 7) != 0) {
+        printf("Data error\n");
+    }
+
+
+    printf("DATA 2: %s\n", &out[512]);
     
-    printf("Hello\n");
+    printf("Hello: %x\n", out);
 }
